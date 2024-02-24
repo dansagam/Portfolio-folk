a7351158b4c32310acc747441a71b3df590dbf9f
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-02-24 09:24:42 +0100

    fix: update the download

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ import { CgGitFork } from "react-icons/cg";
 // import { ImBlog } from "react-icons/im";
 import { AiFillStar, AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
 
-import { CgFileDocument } from "react-icons/cg";
+// import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
